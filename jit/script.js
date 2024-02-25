c9b02ae3fad15527b641805420ba63f32ee3e17e
Sadiya Tahsin Faruquddin Ansari <146445777+SadiyaTahsin29@users.noreply.github.com>
2024-02-25 20:08:39 +0530

    Delete jit directory

--- jit/script.js
+++
@@ -1,7 +0,0 @@
-// Responsive navigation toggle
-const navToggle = document.querySelector('.navbar-toggler');
-const navLinks = document.querySelector('.navbar-nav');
-
-navToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
